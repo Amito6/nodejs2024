820e397d1c61a238d8066d92ef672fe39b535376
amito6 <amitchain24@gmail.com>
2024-06-09 22:23:21 -0400

    commit-04

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
 const http = require("http");
+const queryString = require("querystring");
 const server = http.createServer((request,response)=>{
-    let date = new Date();
-    let c_date = date.toTimeString();
-    response.writeHead(200,{
-        "content-Type" : "application/json"
-    })
-    const res = JSON.stringify({
-        date : c_date
-    });
-    response.write(res)
-    response.end();
+    if(request.method == "GET"){
+        let {username,password} = queryString.parse(request.url.replace("/?",""));
+        if(username == "a@gmail.com" && password == "12345"){
+            response.writeHead(200,{
+                "Content-Type" : "application/json"
+            });
+            response.write(JSON.stringify({
+                message : "Login SuccessFull",
+                username,
+                password
+            })
+        );
+            response.end();
+        }
+        else{
+            response.writeHead(401,{
+                "Content-Type" : "application/json"
+            });
+            response.write(JSON.stringify({
+                message : "User Not Authorized",
+            })
+        );
+            response.end();
+        }
+    }/* post method */
+    else{
+        let userData = "";
+        request.on("data",(chunks)=>{
+            userData += chunks;
+        })
+        request.on("end",()=>{
+            const {username,password} = queryString.parse(userData);
+            if(username == "a@gmail.com" && password == "12345"){
+                response.writeHead(200,{
+                    "Content-Type" : "application/json"
+                });
+                response.write(JSON.stringify({
+                    message : "Login SuccessFull",
+                    username,
+                    password
+                })
+            );
+                response.end();
+            }
+            else{
+                response.writeHead(401,{
+                    "Content-Type" : "application/json"
+                });
+                response.write(JSON.stringify({
+                    message : "User Not Authorized",
+                })
+            );
+                response.end();
+            }
+        })
+    }
 
     
 });
