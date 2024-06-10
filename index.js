e7e6a352aca20e89549ad5cbdb415663e71ffe35
amito6 <amitchain24@gmail.com>
2024-06-09 23:40:29 -0400

    commit-06 MD

--- index.js
+++ index.js
@@ -1,68 +1,80 @@
 const http = require("http");
-const queryString = require("querystring");
+const mongodb = require("mongodb").MongoClient;
+const url = "mongodb://127.0.0.1:27017";
 const server = http.createServer((request,response)=>{
-    if(request.method == "GET"){
-        let {username,password} = queryString.parse(request.url.replace("/?",""));
-        if(username == "a@gmail.com" && password == "12345"){
-            response.writeHead(200,{
-                "Content-Type" : "application/json",
-                'Access-Control-Allow-Origin' : "*"
-            });
-            response.write(JSON.stringify({
-                message : "Get requested Login SuccessFull",
-                username,
-                password
-            })
-        );
-            response.end();
-        }
-        else{
-            response.writeHead(401,{
-                "Content-Type" : "application/json",
-                'Access-Control-Allow-Origin' : "*"
-            });
-            response.write(JSON.stringify({
-                message : "User Not Authorized",
-            })
-        );
-            response.end();
-        }
-    }/* post method */
-    else{
-        let userData = "";
-        request.on("data",(chunks)=>{
-            userData += chunks;
-        })
-        request.on("end",()=>{
-            const {username,password} = queryString.parse(userData);
-            if(username == "a@gmail.com" && password == "12345"){
-                response.writeHead(200,{
-                    "Content-Type" : "application/json",
-                    'Access-Control-Allow-Origin' : "*"
-                });
-                response.write(JSON.stringify({
-                    message : "Post requested Login SuccessFull",
-                    username,
-                    password
-                })
-            );
-                response.end();
-            }
-            else{
-                response.writeHead(401,{
-                    "Content-Type" : "application/json",
-                    'Access-Control-Allow-Origin' : "*"
-                });
-                response.write(JSON.stringify({
-                    message : "User Not Authorized",
-                })
-            );
-                response.end();
-            }
-        })
+    const data = {
+        name : "a@gmail.com",
+        password : "12345"
     }
+    mongodb.connect(url).then((conn) => {
+       const db = conn.db();
+       db.createCollection("users").then((coll) => {
+            console.log("Collection Created");
+            db.collection("users").insertOne(data).then((dataRes) => {
+                response.writeHead(200,{
+                    "Content-Type" : "application/json"
+                });
+                const res = JSON.stringify({
+                    message : "Data Crteated",
+                    data : dataRes
+                });
+                console.log(res)
+                response.write(res);
+                response.end();
 
-    
+            }).catch((err) => {
+                console.log("No Data Created",err)
+            });
+       }).catch((err) => {
+        console.log("Error while creating collection",err)
+       });
+    }).catch((err) => {
+        response.write("While making connection wiuth Mongodb");
+        response.end();
+    });
 });
 
 server.listen(5020);
+
+
+
+/* const http = require("http");
+const mongodb = require("mongodb").MongoClient;
+const url = "mongodb://127.0.0.1:27017";
+const server = http.createServer((request,response)=>{
+    const data = {
+        name : "a@gmail.com",
+        password : "12345"
+    }
+    mongodb.connect(url).then((conn)=>{
+        const db = conn.db("just");
+        db.createCollection("users").then((result) => {
+            db.collection("users").insertOne(data).then((result) => {
+                response.writeHead(200,{
+                    "Content-Type" : "application/json"
+                });
+                const res = JSON.stringify({
+                    message : "DATA Inserted"
+                });
+                response.write("success");
+                response.end();
+            }).catch((err) => {
+                response.writeHead(500,{
+                    "Content-Type" : "application/json"
+                });
+                const res = JSON.stringify({
+                    message : "Inetrnal Server Error while adding data, please contact the admin"
+                });
+                response.write(res);
+                response.end();
+            });
+        }).catch((err) => {
+            console.log("Error while creating collection",error)
+        });
+    }).catch((error)=>{
+        console.log("Error While connecting with database",error)
+    });
+    response.end();
+});
+
+server.listen(5020); */
