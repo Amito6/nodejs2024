dcb72684aaf01ad454d35ade3304c4f74cf5331f
amito6 <amitchain24@gmail.com>
2024-06-09 21:34:21 -0400

    commit-02

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const http = require("http");
 const server = http.createServer((request,response)=>{
-    console.log("success");
-    response.end()
+    let username = "a@gmaidl.com";
+    if(username == "a@gmail.com"){
+        response.writeHead(200,{
+            "Content-Type" : "application/json"
+        });
+        const res = JSON.stringify({
+            message : "login Successfull"
+        });
+        response.write(res);
+        response.end()
+    }
+    else{
+        response.writeHead(401,{
+            "Content-Type" : "application/json"
+        });
+        const res = JSON.stringify({
+            message : "User Not Authenticated"
+        });
+        response.write(res);
+        response.end()
+    }
+    
 });
 
 server.listen(5020);
